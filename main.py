727c50302f8b8043b1df14dc00e6dc381558256f
Mariia Liubiva <mliubiva@gmail.com>
2022-09-13 19:26:10 +0700

    classes

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import pytorch_lightning as pl
-print('Hello World')
+class Animal:
+    name: object
 
-x = 1
-y = x * 2
-z = [x for x in range(0, y - 1)]
-print(x)
+    def __init__(self, name):
+        self.name = name
+        self.age = 2
+
+    def speak(self):
+        print(self.age)
+        print('i am an animal')
+
+
+class Dog(Animal):
+    def speak(self):
+        print('i am a dog')
+
+
+class Bear(Animal):
+    def speak(self) -> object:
+        print('i am a bear named', self.name)
+
+
+animal = Animal(name='fluffy')
+dog = Dog(name='fido')
+bear_1 = Bear(name='baloo')
+bear_2 = Bear(name='yogi')
+animal.speak()
+dog.speak()
+bear_1.speak()
+bear_2.speak()
